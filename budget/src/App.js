216969029e8c973168fb27a77146d2a75c31511c
SrividyaChowdary <32638060+SrividyaChowdary@users.noreply.github.com>
2021-03-18 14:57:27 +0530

    Code optimized

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,98 +1,28 @@
-import { Button, Container, Form, Grid, Header, Icon, Segment, Statistic } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
+import MainHeader from './components/MainHeader';
+import NewEntryForm from './components/NewEntryForm';
 
 function App() {
   return (
    <Container>
 
-     <Header as='h1'>
-       Budget
-     </Header>
+     <MainHeader title='Budget'/>
+     <DisplayBalance title='Your Balance' value='25,345.00' size='small'/>
 
-     <Statistic size="small">
-       <Statistic.Label>Your Balance:</Statistic.Label>
-       <Statistic.Value>25,899</Statistic.Value>
-     </Statistic>
+     <DisplayBalances/>
+     <MainHeader title='History' type='h3'/>
 
-     <Segment textAlign='center'>
-       <Grid columns={2} divided>
-         <Grid.Row>
-           <Grid.Column>
-             <Statistic size='tiny' color='green'>
-               <Statistic.Label styel={{textAlign: 'left'}}>
-                 Income:
-               </Statistic.Label>
-               <Statistic.Value>12,345</Statistic.Value>
-             </Statistic>
-           </Grid.Column>
-           <Grid.Column>
-           <Statistic size='tiny' color='red'>
-               <Statistic.Label styel={{textAlign: 'left'}}>
-                 Expenses:
-               </Statistic.Label>
-               <Statistic.Value>1,345</Statistic.Value>
-             </Statistic>
-           </Grid.Column>
-         </Grid.Row>
-       </Grid>
-     </Segment>
+     <EntryLine description='income' value='$450.00'/>
+     <EntryLine description='expense' value='$120.00' isExpense/>
+     {/* <EntryLine description='income' value='$1,340.00'/>
+     <EntryLine description='expense' value='$80.00' isExpense/> */}
 
-     <Header as='h3'>
-       History
-     </Header>
-
-     <Segment color='red'>
-       <Grid columns={3} textAlign='right'>
-        <Grid.Row>
-          <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
-          <Grid.Column width={3} textAlign='right'>$787878</Grid.Column>
-          <Grid.Column width={3}>
-            <Icon name='edit' bordered />
-            <Icon name='trash' bordered />
-          </Grid.Column>
-        </Grid.Row>
-       </Grid>
-     </Segment>
-
-     <Segment color='green'>
-       <Grid columns={3} textAlign='right'>
-        <Grid.Row>
-          <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
-          <Grid.Column width={3} textAlign='right'>$787878</Grid.Column>
-          <Grid.Column width={3}>
-            <Icon name='edit' bordered />
-            <Icon name='trash' bordered />
-          </Grid.Column>
-        </Grid.Row>
-       </Grid>
-     </Segment>
-
-     <Segment color='red'>
-       <Grid columns={3} textAlign='right'>
-        <Grid.Row>
-          <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
-          <Grid.Column width={3} textAlign='right'>$787878</Grid.Column>
-          <Grid.Column width={3}>
-            <Icon name='edit' bordered />
-            <Icon name='trash' bordered />
-          </Grid.Column>
-        </Grid.Row>
-       </Grid>
-     </Segment>
-
-     <Header as='h3'>Add new transaction</Header>
-     <Form unstackable>
-       <Form.Group>
-         <Form.Input icon='tags' width={12} label='Description' placeholder='New tansaction'/>
-         <Form.Input icon='dollar' iconPosition='left' width={4} label='Value' placeholder='100.00'/>
-       </Form.Group>
-       <Button.Group styel={{ marginTop: 20}}>
-         <Button>Cancel</Button>
-         <Button.Or />
-         <Button>Ok</Button>
-       </Button.Group>
-     </Form>
-
+     <MainHeader title='Add new transaction' type='h3'/>
+     <NewEntryForm/>
    </Container>
   );
 }
