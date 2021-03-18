347a54198ea89812594905f151c8d0ff92a7f20b
SrividyaChowdary <32638060+SrividyaChowdary@users.noreply.github.com>
2021-03-18 22:13:00 +0530

    added add and delete operations

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,30 +1,67 @@
+import { useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import './App.css';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
-import EntryLine from './components/EntryLine';
+import EntryLines from './components/EntryLines';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  function deleteEntry(id){
+    const result = entries.filter(entry => entry.id !== id);
+    setEntries(result);
+
+  }
+
+  function addEntry(description, value){
+    const result = entries.concat({id: entries.length+1, description, value});
+    setEntries(result)
+  }
   return (
    <Container>
-
      <MainHeader title='Budget'/>
      <DisplayBalance title='Your Balance' value='25,345.00' size='small'/>
 
      <DisplayBalances/>
      <MainHeader title='History' type='h3'/>
 
-     <EntryLine description='income' value='$450.00'/>
-     <EntryLine description='expense' value='$120.00' isExpense/>
-     {/* <EntryLine description='income' value='$1,340.00'/>
-     <EntryLine description='expense' value='$80.00' isExpense/> */}
+     <EntryLines entries={entries} deleteEntry={deleteEntry}/>
 
      <MainHeader title='Add new transaction' type='h3'/>
-     <NewEntryForm/>
+     <NewEntryForm addEntry={addEntry}/>
    </Container>
+
   );
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    id:1,
+    description:"Work Income",
+    value:"$1000.00",
+    isExpense:false
+  },
+  {
+    id:2,
+    description:"Water Bill",
+    value:"$100.00",
+    isExpense:true
+  },
+  {
+    id:3,
+    description:"Rent",
+    value:"$300.00",
+    isExpense:true
+  },
+  {
+    id:4,
+    description:"Electricity Bill",
+    value:"$100.00",
+    isExpense:true
+  }
+]
--- budget/src/components/NewEntryForm.js
+++ budget/src/components/NewEntryForm.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Form} from 'semantic-ui-react';
 import ButtonSaveOrCancel from './ButtonSaveOrCancel';
 
-function NewEntryForm() {
+function NewEntryForm({addEntry}) {
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('')
     return (
         <Form unstackable>
         <Form.Group>
-          <Form.Input icon='tags' width={12} label='Description' placeholder='New tansaction'/>
-          <Form.Input icon='dollar' iconPosition='left' width={4} label='Value' placeholder='100.00'/>
+          <Form.Input 
+          icon='tags' 
+          width={12} 
+          label='Description' 
+          placeholder='New tansaction' 
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          />
+          <Form.Input 
+          icon='dollar' 
+          iconPosition='left' 
+          width={4} 
+          label='Value' 
+          placeholder='100.00' 
+          value={value}
+          onChange={(event) => setValue(event.target.value)}/>
         </Form.Group>
-        <ButtonSaveOrCancel/>
+        <ButtonSaveOrCancel addEntry={addEntry} description={description} value={value}/>
       </Form>
     )
 }
